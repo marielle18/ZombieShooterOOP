4325704224650e111bcbfb7563222ffd0cfa26b3
marielle18 <delacruz.marielle@colm.edu.ph>
2025-05-13 23:13:39 +0800

    ZombieFactory.cs

--- ZombieShooterOOP/ZombieFactory.cs
+++ ZombieShooterOOP/ZombieFactory.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace ZombieShooterOOP
 {
     public class ZombieFactory
     {
-        public Enemy CreateEnemy(int x, int y) => new Zombies(x, y);
+        public Enemy CreateEnemy(int x, int y)
+        {
+            return new Zombie(x, y);
+        }
     }
 }
+
+
